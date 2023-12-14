d8fe7d61456d92e05ab969dc9b15fc1457d55168
vishalkr2 <140583311+vishalkr2@users.noreply.github.com>
2023-12-14 08:30:01 -0800

    factorial_2typ.java

--- factorial.java
+++ factorial.java
@@ -1,11 +1,12 @@
 public class factorial {
 
-   static int fact(int n) {
-       if (n == 0) return 1;
+   //Using recursion
+   // static int fact(int n) {
+   //     if (n == 0) return 1;
 
 
-       return n * fact(n - 1);
-   }
+   //     return n * fact(n - 1);
+   // }
 
       // System.out.println(i);
 
@@ -15,15 +16,11 @@ public class factorial {
     public static void main(String args[]){
       // int n=5;
        // System.out.println(fact(5));
-        int s=0;
-        int j=1;//
+       //using loop
+        int j=1;
         int a=5;
         for(int i=1;i<=a;i++){
-           // i=i*a[i];
             j=j*i;
-
-
-          // System.out.println(j);
 
         }
         System.out.println(j);
